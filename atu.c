e01f2b58221c54a833b08dc9d57c9c00ecfcadd6
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-04 01:53:18 -0800

    longer 2d array

--- atu.c
+++ atu.c
@@ -8,20 +8,20 @@
 
 int main(void)
 {
-	int a[2][3], i, j, sum = 0;
+	int a[3][5], i, j, sum = 0;
 
 	printf("Enter the elements of this array\n");
-	for (i = 0; i < 2; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 3; j++)
+		for (j = 0; j < 5; j++)
 		{
 			scanf("%d", &a[i][j]);
 		}
 	}
 	printf("The matrix is:\n");
-	for (i = 0; i < 2; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 3; j++)
+		for (j = 0; j < 5; j++)
 		{
 			printf("%d\t", a[i][j]);
 			sum = sum + a[i][j];

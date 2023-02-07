ca7102d5664c142abcbf3d8895dca7c5ce580532
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-07 13:38:36 -0800

    Masino

---
+++ string1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - prints a string using puts.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	char nm[20] = "Masino";
+
+	printf("%s\n", nm);
+	return (0);
+}

35e95763414ea867bc784707e9e2115063689eab
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-08 13:27:56 -0800

    string2

---
+++ second.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - prints a string.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	char name[20];
+
+	printf("Enter your sister's name here\n");
+	scanf("%s", name);
+	puts(name);
+	return (0);
+}

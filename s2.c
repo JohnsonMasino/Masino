206d0888258ba4f7bb398f1048a83d338659ea36
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-11 07:07:15 -0800

    Jyk my name

---
+++ s2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - prints a name.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	int name;
+
+	printf("The name of this developer is Masino\n");
+	return (0);
+}

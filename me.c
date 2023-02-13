37765e549dd939e634b83acb0c4c289bc391c4a3
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-13 10:56:50 -0800

    questions

---
+++ me.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - prints lines of questions.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Hi!\nWhat is your name?\n");
+	printf("Tell us where you are from\nAre you single or married?\n");
+	printf("Who is gonna be your valentine tomorrow?\n");
+	printf("\nQuestions by Masino@git\n");
+	return (0);
+}

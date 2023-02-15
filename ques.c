62ed8bdd7accdacd661a207399b831161874c5a4
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-15 11:39:11 -0800

    first code

---
+++ ques.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints some questions.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Tell us your name\n");
+	printf("Run the next program\nCode by Masino\n");
+	return (0);
+}

59c94bbf29c556ee046dbc8d2cbaca23701ec80b
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-14 23:45:08 -0800

    time check

---
+++ time.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints the time.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Today I will find out if I can write a code that prints the time\nCode by Masino...\n");
+	return (0);
+}

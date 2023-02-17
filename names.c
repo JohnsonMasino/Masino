9c2f2d4e3dfa402411e30048decb39fc26d10954
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-17 12:17:59 -0800

    User's name

---
+++ names.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints this user's name.
+ * Retrurn: 0.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("The name of this user is JOHNSON MASINO OBINNA\n");
+	return (0);
+}

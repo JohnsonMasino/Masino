1c9467505b8524012789e06c0e219a44430560bb
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-18 15:47:27 -0800

    updated

--- arrfunc.c
+++ arrfunc.c
@@ -10,9 +10,10 @@ int avg(int [], int);
 
 int main(void)
 {
-	int mk[] = {2, 3, 4, 5, 6, 7, 8, 9}, i, average;
+	int mk[5], i, average;
 
-	average = avg(mk, 8);
+	printf("Enter five numbers here:\n");
+	average = avg(mk, 5);
 	printf("The average is %d\n", average);
 	printf("\nCode developed by Masino\n");
 }
@@ -21,8 +22,9 @@ int avg(int mk[], int len)
 {
 	int i, sum = 0, average = 0;
 	
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 5; i++)
 	{
+		scanf("%d", &mk[i]);
 		sum = sum + mk[i];
 	}
 	printf("Sum is:\n%d\n", sum);

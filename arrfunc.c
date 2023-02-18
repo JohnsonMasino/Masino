0118b37ce6197b325c388800a5f23eedbf5c8e7f
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-18 15:31:42 -0800

    array of function

---
+++ arrfunc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+ * main - prints array of integer.
+ * Return: 0.
+ * Code by Masino.
+ */
+
+int avg(int [], int);
+
+int main(void)
+{
+	int mk[] = {2, 3, 4, 5, 6, 7, 8, 9}, i, average;
+
+	average = avg(mk, 8);
+	printf("The average is %d\n", average);
+	printf("\nCode developed by Masino\n");
+}
+
+int avg(int mk[], int len)
+{
+	int i, sum = 0, average = 0;
+	
+	for (i = 0; i < 8; i++)
+	{
+		sum = sum + mk[i];
+	}
+	average = sum / len;
+	return (average);
+}

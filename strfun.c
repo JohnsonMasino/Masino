fcc758168e8eda349edd90f73f76018f954de42f
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-18 16:40:18 -0800

    two strings

--- strfun.c
+++ strfun.c
@@ -11,20 +11,22 @@ int str(char [], int);
 
 int main(void)
 {
-	char nm[10];
-	int length, len;
+	char nm[10], nm1[10];
+	int length,length1, len, len1;
 
 	printf("Enter a name here:\n");
 	length = str(nm, len);
-	printf("The length is:\n%d\n", length);
+	length1 = str(nm1, len1);
+	printf("The length of first is:\n%d\n", length);
+	printf("The length of second is:\n%d\n", length1);
 	printf("\nCode developed by Masino\n");
 	return (0);
 }
 
 int str(char name[], int a)
 {
-	char nm[10];
-	int i, len = 0;
+	char nm[10], nm1[10];
+	int i, j, len = 0, len1 = 0;
 
 	//len = strlen(nm);
 	scanf("%s", nm);
@@ -32,5 +34,7 @@ int str(char name[], int a)
 	{
 		len = len + 1;
 	}
+	for (j = 0; nm1[j] != '\0'; j++)
+		len1 = len1 + 1;
 	return (len);
 }

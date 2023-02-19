22151836499ef56874bc2bc9db17776b6ed9d743
JohnsonMasino <johnsonmasino@gmail.com>
2023-02-18 16:43:55 -0800

    quote

---
+++ nm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Prints the name of this user.
+ * Code by Masino.
+ */
+
+int main(void)
+{
+	printf("Resilience is the key to success\nWe keep going even if it gets tougher...\n");
+	printf("\nCode developed by Masino\n");
+	return (0);
+}
